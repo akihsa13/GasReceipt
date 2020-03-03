72471a118ed599cf27be0a3a2de4caea7d9a2a95
Ashika <ashika.baraili.2023@student.lhschools.org>
2020-03-03 14:00:55 -0500

    Intellij commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-----------------------------------+");
+
+        System.out.println("|                                   |");
+        System.out.println("|           WELCOME TO              |");
+        System.out.println("|        ACE GAS STATION            |");
+        System.out.println("|        FUEL UP YOUR CAR           |");
+        System.out.println("|       HOUSTON, TX 77038           |");
+        System.out.println("|         281-546-8952              |");
+
+
+
+        System.out.println("|   09/25/18            07:14:33    |");
+        System.out.println("|   PUMP                Reg 3       |");
+        System.out.println("|   INVOICE            3014489      |");
+        System.out.println("|   GALLON             30.98        |");
+        System.out.println("|   PRICE/GAL   $      2.14         |");
+        System.out.println("|   FUEL SALE   $      66.30        |");
+
+        System.out.println("|    CASH TEND   $      100.00      |");
+        System.out.println("|    CHANGE      $      0.00        |");
+
+
+
+        System.out.println("|      Ace/gas.station.com          |");
+        System.out.println("|  THANK YOU FOR YOUR ATTENDENT     |");
+        System.out.println("|      PLEASE COME AGAIN!!!         |");
+
+
+
+        System.out.println("|+---------------------------------+|");
+
+
+
+
+
+    }
+}
